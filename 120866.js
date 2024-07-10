9b0103e2a7d043e3f22602e9793dcc44bcd01825
Jin-devST <kpasd002@gmail.com>
2024-07-10 22:19:01 +0900

    77884.js, 120866.js ,150370.js

--- 120866.js
+++ 120866.js
@@ -1,14 +1,23 @@
 function solution(board) {
     var answer = 0;
-    for (let i = 0; i < board.length; i++) {
+    const length = board.length
+    for (let i = 0; i < length; i++) {
         if (!board[i].includes(1)) {
             continue;
         }
-        for (let j = 0; j < board[i].length; j++) {
-            if (board[i][j] === 0) {
+        for (let j = 0; j < length; j++) {
+            if (board[i][j] !== 1) {
                 continue;
             }
-            if (board[i][j] === 1) {
+            if (i === 0 && j === 0) {
+                board[i + 1][j + 1] !== 1 ? board[i + 1][j + 1] = 2 : 1
+                board[i + 1][j] !== 1 ? board[i + 1][j] = 2 : 1
+                board[i][j + 1] !== 1 ? board[i][j + 1] = 2 : 1
+            } else if (i === 0 && j === length - 1) {
+
+            } else if (i === length - 1 && j === 0) {
+            } else if (i === length - 1 && j === length - 1) {
+            } else {
                 board[i + 1][j + 1] !== 1 ? board[i + 1][j + 1] = 2 : 1
                 board[i + 1][j - 1] !== 1 ? board[i + 1][j - 1] = 2 : 1
                 board[i - 1][j + 1] !== 1 ? board[i - 1][j + 1] = 2 : 1
@@ -17,12 +26,21 @@ function solution(board) {
                 board[i + 1][j] !== 1 ? board[i + 1][j] = 2 : 1
                 board[i - 1][j] !== 1 ? board[i - 1][j] = 2 : 1
                 board[i][j + 1] !== 1 ? board[i][j + 1] = 2 : 1
-                board[i][j - 1] !== 1 ? board[i][j + 1] = 2 : 1
+                board[i][j - 1] !== 1 ? board[i][j - 1] = 2 : 1
             }
         }
     }
-    return answer;
+    return (length ** 2) - board.reduce((acc, cur) => {
+        acc += cur.reduce((acc, cur) => {
+            if (cur > 0) {
+                return acc + 1
+            }
+            return acc
+        }, 0)
+        return acc
+    }, 0)
 }
 
 const board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]
+// const board = [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
 console.log(solution(board)); //16
